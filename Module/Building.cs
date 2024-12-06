b8a97ff8dc4ba992b1ab0c10c3149afa1fd84312
Jesper vd Tol <jvandertol@student.scalda.nl>
2024-12-06 12:31:12 +0100

    added room to createtask and edited splashscreen and loadingscreen

---
+++ Module/Building.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareCare.Module
+{
+    public class Building
+    {
+        public int BuildingID { get; set; }
+        public string Name { get; set; }
+        public List<Room> Rooms { get; set; } = new List<Room>();
+    }
+}
