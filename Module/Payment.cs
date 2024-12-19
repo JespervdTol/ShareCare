765a78766cd20a19f97be3eb3502ebd472612ba9
Jesper vd Tol <I562011@fontysict.nl>
2024-12-19 17:49:07 +0100

    added payments page

---
+++ Module/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShareCare.Module
+{
+    public class Payment
+    {
+        public int PaymentID { get; set; }
+        public decimal Amount { get; set; }
+        public string Summary { get; set; }
+        public int PeopleAmount { get; set; }
+        public string Link { get; set; }
+        public int TaskID { get; set; }
+        public List<int> UserIDs { get; set; } = new List<int>();
+    }
+}
